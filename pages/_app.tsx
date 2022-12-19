56252b47d38693c20290be7725e97180ceda79b6
SatooRu65536 <ryuto4619@gmail.com>
2022-12-20 08:19:55 +0900

    :sparkles: フォントを変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Noto_Sans_JP } from '@next/font/google'
+
+const noto = Noto_Sans_JP({
+  weight: ['400', '700'],
+  subsets: ['japanese'],
+  style: ['normal']
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${noto.style.fontFamily};
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
